fb860dc4d0bf05b33f1077e96879624930f5ea9e
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-03-29 20:27:35 +0200

    trying testing and add data.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App without crash', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  let firstElement = screen.getByRole('h2');
+  expect(firstElement).toBeInTheDocument();
 });
