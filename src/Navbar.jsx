801b2f3cdf33915d297cc3afb69ecb9d6c177927
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-04-04 22:57:00 +0200

    add icons media

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,20 +1,43 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
 
 const Navbar = () => {
   return (
     <nav>
-      <div className='nav-header'>header</div>
-      <div className='nav-toggle'>toggle</div>
-      <div className='links-container'>
-        {links.map(link => {
-          const {id, url, text} = link
-          return <a className='links' key={id} href={url}>{text}</a>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" />
+          <button className='nav-toggle'> <FaBars /></button>
+        </div>
+      </div>
+      <div className="links-container show-container">
+        <ul className="links">
+        {links.map((link) => {
+          const { id, url, text } = link;
+          return (
+            <li>
+              <a key={id} href={url}>
+                {text}
+              </a>
+            </li>
+          );
         })}
+        </ul>
+        <ul className="social-icons">
+        {social.map((rrss) => {
+          const { id, url, icon } = rrss;
+          return (
+            <li>
+              <a key={id} href={url}>{icon}</a>
+            </li>
+          );
+        })}
+        </ul>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
